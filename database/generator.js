42bbba74ac0883436b3f14642188368e97cc1cca
Lawrence Chou <lawrencechou@Lawrences-MacBook-Pro.local>
2019-03-01 19:57:19 -0800

    Finish Seeding script of 10M

--- database/generator.js
+++ database/generator.js
@@ -1,47 +1,88 @@
 var fs = require("fs");
 var faker = require('faker');
 // var csvWriter = require('csv-write-stream');
-var headers = ['res_name', 'ratings_num', 'rating', 'descript', 'price_min', 'price_max', 'food_type', 'tag_one', 'tag_two', 'tag_three', 'neighborhood', 'brunch_hrs', 'lunch_hrs', 'dinner_hrs', 'dining_style', 'dress_code', 'payment_options', 'chef', 'entertainment', 'additional_info', 'website', 'phone_number', 'street_address']
+var headers = ['res_name', 'ratings_num', 'rating', 'descript', 'price_min', 'price_max', 'food_type','city', 'lunch_hrs', 'dinner_hrs','dress_code', 'payment_options', 'chef', 'entertainment', 'additional_info', 'website', 'phone_number', 'street_address']
 
-// `res_name,ratings_num,rating,descript,price_min,price_max,food_type,tag_one,tag_two,tag_three,neighborhood,brunch_hrs,lunch_hrs,dinner_hrs,dining_style,dress_code,payment_options,chef,entertainment,additional_info,website,phone_number,street_address`
+var foods = ['Mexican', 'Italian', 'Korean', 'American', 'Russian', 'Chilean', 'Chinese', 'Japanese', 'Indonesian', 'German']
 
-const writetenMillion = (dest, data, n) => {
+var dress = ['nude', 'Business Casual', 'Casual', 'Semi-Formal', 'Beach Formal']
+
+var payment = ['Venmo', 'Cash', 'Credit','IOU']
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getRndFood () {
+  return foods[getRndInteger(0, 9)]
+}
+
+function getRndPay(){
+  return payment[getRndInteger(0,3)]
+}
+
+function getRndCode () {
+  return dress[getRndInteger(0,4)]
+}
+
+var data = () => {
+  const res_name = faker.name.findName()
+  const ratings_num = getRndInteger(0,5)
+  const rating = 5
+  const descript = faker.lorem.sentences()
+  const price_min = getRndInteger(10,30)
+  const price_max = getRndInteger(50,120)
+  const food_type = getRndFood()
+  const city = faker.address.city()
+  const lunch_hrs = getRndInteger(1030,1430)
+  const dinner_hrs = getRndInteger(1700,2200)
+  const dress_code = getRndCode()
+  const payment_options = getRndPay()
+  const chef = faker.name.findName()
+  const entertainment = faker.lorem.sentence()
+  const additional_info = faker.lorem.sentences()
+  const website = faker.internet.url()
+  const phone_number = faker.phone.phoneNumber()
+  const street_address = faker.address.streetAddress()
+
+  return `'${res_name}',${ratings_num},${rating}, '${descript}', ${price_min}, ${price_max}, ${food_type},'${city}', ${lunch_hrs}, ${dinner_hrs}, ${dress_code}, ${payment_options}, '${chef}', '${entertainment}', '${additional_info}', '${website}', ${phone_number}, '${street_address}'\n`
+
+
+}
+
+
+
+const writetenMillion = (dest, data, num) => {
 
 
   var writer = fs.createWriteStream(dest);
   var header = headers.join(',') + '\n'
   writer.write(header, 'utf8')
-  let i = n;
+  let i = 0;
   write();
   function write() {
     let ok = true;
     do {
-      i--;
-      if (i === 0) {
+      i++;
+      if (i === num) {
         // last time!
-        writer.write(data, 'utf8');
+        writer.write(data(), 'utf8');
       } else {
       
-        ok = writer.write(data, 'utf8');
+        ok = writer.write(data(), 'utf8');
       }
-    } while (i > 0 && ok);
-    if (i > 0) {
+    } while (i < num && ok);
+    if (i < num) {
     
       writer.once('drain', write);
     }
   }
 
-  writer.end()
-  writer.on(
-    "finish",
-    ()=> {
-        console.log("It worked");
-    }
-  );
+
 
 }
 
-writetenMillion('./test1.csv', `'name', 4, 5, 'not bad', 15,20, 'ba', 'asd', 'asd', 'asd', 'er', 10, 11, 12, 'es', 'asd', 'asdd', 'err', 'err', 'asd', 'ee', 67676, 'asd'\n`, 10)
+writetenMillion('./test1.csv', data, 10000000)
 
 // const streamData = function() {
 //   var writer = csvWriter({ headers: headers})
--- database/schema.sql
+++ database/schema.sql
@@ -4,6 +4,8 @@ CREATE DATABASE overview;
 
 \c overview;
 
+\timing
+
 DROP TABLE IF EXISTS listings;
 
 CREATE TABLE listings (
@@ -15,25 +17,20 @@ CREATE TABLE listings (
 	price_min INT,
 	price_max INT,
 	food_type TEXT,
-	tag_one TEXT,
-	tag_two TEXT,
-	tag_three TEXT,
-	neighborhood TEXT,
-	brunch_hrs INT,
+	city TEXT,
 	lunch_hrs INT,
 	dinner_hrs INT,
-	dining_style TEXT,
 	dress_code TEXT,
 	payment_options TEXT,
 	chef TEXT,
 	entertainment TEXT,
 	additional_info TEXT,
 	website TEXT,
-	phone_number INT,
+	phone_number VARCHAR(25),
 	street_address TEXT
 );
 
 
 
-\COPY listings(res_name, ratings_num, rating, descript, price_min, price_max, food_type, tag_one, tag_two, tag_three, neighborhood, brunch_hrs, lunch_hrs, dinner_hrs, dining_style, dress_code, payment_options, chef, entertainment, additional_info, website, phone_number, street_address) FROM 'test1.csv' DELIMITER ',' CSV HEADER;
+\COPY listings(res_name, ratings_num, rating, descript, price_min, price_max, food_type, city,  lunch_hrs, dinner_hrs, dress_code, payment_options, chef, entertainment, additional_info, website, phone_number, street_address) FROM 'test1.csv' DELIMITER ',' CSV HEADER;
 
